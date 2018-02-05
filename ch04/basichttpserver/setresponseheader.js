7c47c99fd228240d5c50dd356123987a55369881
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-05 20:20:02 +0900

    4.1: basic http server

---
+++ ch04/basichttpserver/setresponseheader.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer(function(req, res){
+	var body = 'Hello World';
+	res.setHeader('Content-Length', body.length);
+	res.setHeader('Content-Type', 'text/plain');
+	res.end(body);
+});
+server.listen(3000);
