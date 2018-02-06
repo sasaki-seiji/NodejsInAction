8d566d5ece16e17b0f32637d46bd2496406b4765
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-06 20:56:10 +0900

    4.2.2: set Content-Length header

--- ch04/restful/todo.js
+++ ch04/restful/todo.js
@@ -16,10 +16,12 @@ var server = http.createServer(function(req, res) {
 			});
 			break;
 		case 'GET':
-			items.forEach(function(item, i) {
-				res.write(i + ') ' + item + '\n');
-			});
-			res.end();
+			var body = items.map(function(item, i) {
+				return i + ') ' + item + '\n';
+			}).join('\n');
+			res.setHeader('Content-Length', Buffer.byteLength(body));
+			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+			res.end(body);
 			break;
 	}
 });
