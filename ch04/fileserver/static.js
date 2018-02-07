68533be7f6ca56cc12de7975ebad13f0f0318a7f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-07 19:25:53 +0900

    4.3.2: handle server error

--- ch04/fileserver/static.js
+++ ch04/fileserver/static.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(req, res){
 	var path = join(__dirname, url.pathname);
 	var stream = fs.createReadStream(path);
 	
-/* optimize data transfer using Stream#pipe()
+/* 4.3.1: optimize data transfer using Stream#pipe()
 	stream.on('data', function(chunk){
 		res.write(chunk);
 	});
@@ -19,6 +19,12 @@ var server = http.createServer(function(req, res){
 	});
 */
 	stream.pipe(res);
+	// 4.3.2: handle server error
+	stream.on('error', function(err) {
+		res.statusCode = 500;
+		res.end('Internal Server Error');
+	});
+	
 });
 
 server.listen(3000);
